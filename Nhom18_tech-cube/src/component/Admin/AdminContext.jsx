5dfe9fa668190bc14a9de0f71e6bcd98dd490a21
DLcMinhNhat12 <tdmnhatt@gmail.com>
2023-10-06 01:32:51 +0700

    second

--- Nhom18_tech-cube/src/component/Admin/AdminContext.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
-
-const AdminContext = ({children}) => {
-    const auth = JSON.parse(localStorage.getItem('adminAuth')) || false
-  return !auth? <Navigate to='/adminlogin' /> : children
-}
-
-export default AdminContext
